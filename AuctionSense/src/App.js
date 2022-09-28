4d2d0ed123a0369119053a5d02a3f5fc7c5036cf
JarnoStudent <481737@student.fontys.nl>
2022-09-28 15:56:24 +0200

    Added React routing

--- AuctionSense/src/App.js
+++ AuctionSense/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Games from "./pages/games/Games";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/games">Games</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<Games />} />
+      </Routes>
+    </>
   );
 }
 
