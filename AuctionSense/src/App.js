5a4f1dd6999697a446733a37b6c4506f59a5509a
Jarno Weemen <101181692+jarnoweemen@users.noreply.github.com>
2022-10-05 16:52:13 +0200

    Revert "Feature 0001 get all items from api"

--- AuctionSense/src/App.js
+++ AuctionSense/src/App.js
@@ -1,27 +1,9 @@
-import "./App.css";
-import { Link, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Games from "./pages/Games";
-import React from "react";
+import './App.css';
 
 function App() {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/games">Games</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/games" element={<Games />} />
-      </Routes>
-    </>
+    <div className="App">
+    </div>
   );
 }
 
