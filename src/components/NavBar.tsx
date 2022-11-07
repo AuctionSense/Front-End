960a4286db9bfbfa39c456455794169851cd7a36
Jarno Weemen <jarno.weemen@gmail.com>
2022-11-07 11:47:59 +0100

    Dynamic way of showing available categories in navbar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,51 +1,89 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { isTemplateSpan } from "typescript";
 import styles from "../App.module.css";
+import Category from "../models/Category";
 import KeyCloakService from "../services/KeyCloakService";
+import UseFetchGet from "../services/UseFetchApi";
 import BalanceNavBar from "./BalanceNavBar";
 import LoginButton from "./Buttons/LoginButton";
 import LogoutButton from "./Buttons/logoutButton";
 import UsernameNavBar from "./UsernameNavBar";
 
 function NavBar() {
-  // GET ALL CATEGORIES HERE FROM BACK-END AND SHOW A LINK UNDER CATEGORY
+  const [categories, setCategories] = useState<Category[]>([]);
+  const { data, isLoaded, error } = UseFetchGet("all/categories");
 
-  let button = <LoginButton />; 
+  let button = <LoginButton />;
   let username;
   let addBalance;
-  
-  if (KeyCloakService.isLoggedIn())
-  {
+
+  useEffect(() => {
+    if (data) {
+      setCategories(data);
+    }
+  }, [data]);
+
+  if (KeyCloakService.isLoggedIn()) {
     button = <LogoutButton />;
-    username = <UsernameNavBar />
-    addBalance = <BalanceNavBar />
+    username = <UsernameNavBar />;
+    addBalance = <BalanceNavBar />;
   }
 
-  return (
-    <nav className={styles.navBarTop}>
-      <div>
-        <ul>
-          <li className={styles.navBarTopFront}>
-            <Link className={styles.navBarTopLink} to="/">
-              Image placeholder
-            </Link>
-          </li>
-          <li className={`${styles.dropDown} ${styles.navBarTopFront}`}>
-            <p>Category</p>
-            <div className={styles.dropDownContent}>
-              <Link className={styles.dropDownContentLink} to={`/games`} state={{category: "games"}}>
-                Games
+  if (categories) {
+    return (
+      <nav className={styles.navBarTop}>
+        <div>
+          <ul>
+            <li className={styles.navBarTopFront}>
+              <Link className={styles.navBarTopLink} to="/">
+                Image placeholder
               </Link>
-            </div>
-          </li>
-          <li className={styles.navBarTopEnd}>
-            {button}
-          </li>
-          {username}
-          {addBalance}
-        </ul>
-      </div>
-    </nav>
-  );
+            </li>
+            <li className={`${styles.dropDown} ${styles.navBarTopFront}`}>
+              <p>Category</p>
+              <div className={styles.dropDownContent}>
+                {categories.map((category) => (
+                  <Link
+                    className={styles.dropDownContentLink}
+                    key={category.id}
+                    to={`/${category.name}`}
+                    state={{ category: category.name }}
+                  >
+                    {category.name}
+                  </Link>
+                ))}
+              </div>
+            </li>
+            <li className={styles.navBarTopEnd}>{button}</li>
+            {username}
+            {addBalance}
+          </ul>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className={styles.navBarTop}>
+        <div>
+          <ul>
+            <li className={styles.navBarTopFront}>
+              <Link className={styles.navBarTopLink} to="/">
+                Image placeholder
+              </Link>
+            </li>
+            <li className={`${styles.dropDown} ${styles.navBarTopFront}`}>
+              <p>Category</p>
+              <div className={styles.dropDownContent}></div>
+            </li>
+            <li className={styles.navBarTopEnd}>{button}</li>
+            {username}
+            {addBalance}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
